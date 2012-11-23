6210729657c0b9a137d114d2b6c7e8455e2ea297
Dan McClain <git@danmcclain.net>
2012-11-23 11:24:34 -0500

    Flips auth urls for github, fixes test suite

--- app/models/identities/oauth2/github.rb
+++ app/models/identities/oauth2/github.rb
@@ -1,3 +1,7 @@
 class Identities::Oauth2::Github < Identities::Oauth2::Base
   extend EasyAuth::Models::Identities::Oauth2::Github
+  def self.authenticate(controller)
+    debugger
+    super
+  end
 end
